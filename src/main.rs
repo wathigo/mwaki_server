44097b79aad509e090126d958bc8dbc050428427
wathigo <wathigosimon@gmail.com>
2023-12-14 15:03:00 +0300

    Add a simple single-threaded server

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::fs::File;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connection established!");
+
+        handleConnection(stream);
+    }
+}
+
+fn handleConnection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (header, file_name) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n{}", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+    
+    let mut f = File::open(file_name).unwrap();
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).unwrap();
+    let response = format!("{}{}", header, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+     
+}
