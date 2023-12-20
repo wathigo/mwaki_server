1dfc85703aa3cbb210dd0b00c975e01ac960bba1
wathigo <wathigosimon@gmail.com>
2023-12-20 19:06:37 +0300

    Simulate a slow request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::thread;
+use std::time::Duration;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -20,9 +22,13 @@ fn handleConnection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (header, file_name) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n{}", "hello.html")
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    }else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
     };
