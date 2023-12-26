65f4d0d534b189a69bcbf2b1f65556f0cb0207ab
wathigo <wathigosimon@gmail.com>
2023-12-26 22:53:12 +0300

    Send jobs fromthred pool down to the receiving end of the workers

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ pub struct ThreadPool {
     sender: mpsc::Sender<Job>,
 }
 
-struct Job;
+type Job = Box<FnOnce() + Send + 'static>;
  
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -40,7 +40,8 @@ impl ThreadPool {
     pub fn execute<F>(&self, f: F) 
         where F: FnOnce() + Send + 'static
         {
-
+            let job = Box::new(f);
+            self.sender.Send(job).unwrap();
         }
 }
 
