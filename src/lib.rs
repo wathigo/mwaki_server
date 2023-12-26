3c436fb8cf21f50e0474aeba4e4c5a29afe63c5d
wathigo <wathigosimon@gmail.com>
2023-12-26 23:35:01 +0300

    Use a walk around syntax to get job out of the box since self: Box<Self> don't work on closures

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,17 @@ pub struct ThreadPool {
     sender: mpsc::Sender<Job>,
 }
 
-type Job = Box<FnOnce() + Send + 'static>;
+trait FnBox {
+    fn call_box(self: Box<Self>);
+}
+
+impl <F: FnOnce()> FnBox for F {
+    fn call_box(self: Box<F>) {
+        (*self)()
+    }
+}
+
+type Job = Box<dyn FnBox + Send + 'static>;
  
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -41,7 +51,7 @@ impl ThreadPool {
         where F: FnOnce() + Send + 'static
         {
             let job = Box::new(f);
-            self.sender.Send(job).unwrap();
+            self.sender.send(job).unwrap();
         }
 }
 
@@ -52,9 +62,13 @@ struct Worker {
 
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(|| {
-            receiver;
+        let thread = thread::spawn(move || {
+            loop {
+                let job = receiver.lock().unwrap().recv().unwrap();
+                println!("Worker {:?} got a job executing", job.call_box());
+            }
         });
+
         Worker {
             id,
             thread
